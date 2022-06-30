570c2db87723e197f85ea66ba3adbd8db19561ff
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-30 13:08:30 -0500

    Created layout and added header

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../Header';
+import { Page } from '../../styles/templates';
+
+export const Layout = (props: { children: JSX.Element }) => {
+  return (
+    <Page>
+      <Header />
+      {props.children}
+    </Page>
+  );
+};
