b80abcab75ae5aeb6557fe13d306552effacd3c3
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-30 13:01:24 -0500

    Created component for game card

---
+++ src/components/Game/index.tsx
@@ -0,0 +1,27 @@
+import { GameCard, GameInfo } from './styles';
+
+import { SkeletonLoader } from '../SkeletonLoader';
+import { GameData } from '../../hooks/useGameList';
+import { useImageLoad } from '../../hooks/useImageLoad';
+
+export const Game = (props: { gameData: GameData }) => {
+  const { gameData } = props;
+  const { img, loaded } = useImageLoad(gameData.imageUrl);
+
+  return (
+    <GameCard to={gameData.link}>
+      {loaded ? (
+        <>
+          <img alt={gameData.name} src={img} />
+          <GameInfo>
+            <h3> {gameData.name} </h3>
+            <p> {gameData.info} </p>
+            <gameData.Icon />
+          </GameInfo>
+        </>
+      ) : (
+        <SkeletonLoader />
+      )}
+    </GameCard>
+  );
+};
---
+++ src/components/Game/styles.tsx
@@ -0,0 +1,113 @@
+import { Link } from 'react-router-dom';
+import styled, { css, keyframes } from 'styled-components';
+
+import { DeviceSize } from '../../DeviceSize';
+import { fadeIn } from '../../styles/animation';
+
+const appearKeyframes = keyframes`
+  from {
+    background: none;
+  }
+  to {
+    background: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const appear = css`
+  animation-duration: 0.3s;
+  animation-name: ${appearKeyframes};
+  animation-timing-function: ease-out;
+  animation-iteration-count: 1;
+`;
+
+export const GameCard = styled(Link)`
+  border-radius: 5px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: var(--shadow);
+
+  & > div,
+  > img {
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+  }
+
+  & > img {
+    ${fadeIn()}
+  }
+
+  &:hover,
+  :focus {
+    outline: none;
+    & > div {
+      display: flex;
+    }
+  }
+
+  @media (min-width: ${DeviceSize.laptop}) {
+    height: 220px;
+    width: 220px;
+    & > div {
+      display: none;
+    }
+  }
+
+  @media (max-width: ${DeviceSize.tablet}) {
+    & > div {
+      display: flex;
+    }
+  }
+
+  @media (max-width: ${DeviceSize.tablet}) and (min-width: ${DeviceSize.smallTablet}) {
+    height: 220px;
+    width: 220px;
+  }
+
+  @media (max-width: ${DeviceSize.smallTablet}) {
+  }
+
+  @media (max-width: ${DeviceSize.smallTablet}) and (min-width: ${DeviceSize.mobile}) {
+    height: 200px;
+    width: 200px;
+  }
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    height: 250px;
+    width: 250px;
+  }
+`;
+
+export const GameInfo = styled.div`
+  ${appear};
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  align-items: center;
+  justify-content: space-evenly;
+  color: white;
+
+  & > h3 {
+    font-weight: 500;
+    font-size: 1.1rem;
+    text-transform: capitalize;
+  }
+
+  & > p {
+    font-weight: 400;
+    &::first-letter {
+      text-transform: uppercase;
+    }
+    font-size: 0.9rem;
+  }
+
+  & > svg {
+    font-size: 2rem;
+  }
+`;
