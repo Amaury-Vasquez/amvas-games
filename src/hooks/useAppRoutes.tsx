065518f144b61cddf647e6ba3de56fff7c6e2307
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-07-12 11:20:40 -0500

    Created hook for app routing

---
+++ src/hooks/useAppRoutes.tsx
@@ -0,0 +1,45 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+//lazy imports
+const Home = lazy(() => import('../pages/Home'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+const Pokemory = lazy(() => import('../pages/Pokemory'));
+const TicTacToe = lazy(() => import('../pages/TicTacToe'));
+const RockPaperScissors = lazy(() => import('../pages/RockPaperScissors'));
+
+export const useAppRoutes = () => {
+  const routes = [
+    {
+      element: <Home />,
+      path: '/',
+    },
+    {
+      element: <NotFound />,
+      path: '*',
+    },
+    {
+      element: <Pokemory />,
+      path: '/pokemory',
+    },
+    {
+      element: <TicTacToe />,
+      path: '/tic-tac-toe',
+    },
+    {
+      element: <RockPaperScissors />,
+      path: '/rock-paper-scissors',
+    },
+  ];
+  const mapRoutes = () => {
+    return (
+      <Routes>
+        {routes.map((item, i) => (
+          <Route element={item.element} key={item.path + i} path={item.path} />
+        ))}
+      </Routes>
+    );
+  };
+
+  return mapRoutes;
+};
