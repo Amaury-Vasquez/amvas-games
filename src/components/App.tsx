fa571dd808a2c701bce9bcf5af21d3ebed8c4069
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-07-12 11:19:18 -0500

    Added route hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,21 @@
-import { lazy, Suspense } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 import { Layout } from './Layout';
 import { FallbackScreen } from './FallbackScreen';
+import { useAppRoutes } from '../hooks/useAppRoutes';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
-//lazy imports
-const Home = lazy(() => import('../pages/Home'));
-const NotFound = lazy(() => import('../pages/NotFound'));
-const Pokemory = lazy(() => import('../pages/Pokemory'));
-const TicTacToe = lazy(() => import('../pages/TicTacToe'));
-
 const App = () => {
+  const mapRoutes = useAppRoutes();
+
   return (
     <HelmetProvider>
       <GlobalStyles />
       <BrowserRouter>
         <Layout>
-          <Suspense fallback={<FallbackScreen />}>
-            <Routes>
-              <Route element={<Home />} path="/" />
-              <Route element={<NotFound />} path="*" />
-              <Route element={<Pokemory />} path="/pokemory" />
-              <Route element={<TicTacToe />} path="/tic-tac-toe" />
-            </Routes>
-          </Suspense>
+          <Suspense fallback={<FallbackScreen />}>{mapRoutes()}</Suspense>
         </Layout>
       </BrowserRouter>
     </HelmetProvider>
